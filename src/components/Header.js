a8601c96a07727a492355634455609f44c29f388
Daniel <danielmaestreyepes@gmail.com>
2022-02-10 16:46:24 +0100

    added seacr bar icons and better size and looking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../assets/Elements/logo.png";
+import 'boxicons'
+import wallet from "../assets/Elements/Design-2_0026_Layer-17.png"
 import "../styles/Header.css";
 
 const Header = ()=>{
@@ -8,9 +10,19 @@ const Header = ()=>{
             <div className="sidebar-logo">
                 <img src={logo} style={{height:"8vh"}} />
             </div>
+
             <div className="header-right">
-                <input id='search-game' type="text" placeholder="Search games..." />
-                <button className="connect-wallet">Connect Wallet</button>
+                
+                <div className="search-bar">
+                    <input id='search-game' type="text" placeholder="Search games..." />
+                    <box-icon id="search-game-icon" name='search'></box-icon>
+                </div>
+
+                <div className="connect-wallet">
+                    <img className="connect-wallet-icon" src={wallet}/>
+                    <button className="connect-wallet-button">Connect Wallet</button>
+                </div>
+
             </div>
         </div>
     )
