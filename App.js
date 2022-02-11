2943545aa9b5e76203856ef31d6a6e97fafab280
nn-dmt <75390655+nn-dmt@users.noreply.github.com>
2022-02-10 17:52:59 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import LeftSideBar from './components/LeftSideBar';
+import Main from './components/Main.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
