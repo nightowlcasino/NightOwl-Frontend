8d9c7ce1d52b5f89f46d2c3be85f53ef7d4c5cdd
Hritik Bakshi <hritikb90@gmail.com>
2022-02-18 23:17:33 +0530

    Completed Swap Page

--- src/components/Swap/Swap.js
+++ src/components/Swap/Swap.js
@@ -1,45 +1,49 @@
 import React from "react";
+import image from "../../assets/Elements/swap-desktop.png";
 import "./Swap.css";
 
 const Swap = () => {
 	return (
-		<div className="swap-div">
-			<h1>Swap</h1>
-            <p>Ergo blockchain token swap</p>
-            <div className="initial-swap-token">
-                <div className="custom-select">
-                    <select>
-                        <option value="erg">ERG</option>
-                    </select>
+        <main>
+            <div className="swap-div">
+                <h1>Swap</h1>
+                <p className="desc">Ergo blockchain token swap</p>
+                <div className="initial-swap-token">
+                    <div className="custom-select">
+                        <select>
+                            <option value="erg">ERG</option>
+                        </select>
+                    </div>
+                    
+                    <input type="number" className="token-input" />
                 </div>
-                <br/>
-                <input type="number" className="token-input" />
-            </div>
 
-            <div className="mid-swap">
-                <p>.</p>
-                <p>.</p>
-                <span>To</span>
-                <p>.</p>
-                <p>.</p>
-            </div>
-
-            <div className="final-swap-token">
-                <div className="custom-select">
-                    <select>
-                        <option value="erg">OWL</option>
-                    </select>
+                <div className="mid-swap">
+                    <p>.</p>
+                    <p style={{marginBottom:"8px"}}>.</p>
+                    <p className="to">To</p>
+                    <p>.</p>
+                    <p>.</p>
                 </div>
-                <br/>
-                <input type="number" className="token-input" />
+
+                <div className="final-swap-token">
+                    <div className="custom-select">
+                        <select>
+                            <option value="erg">OWL</option>
+                        </select>
+                    </div>
+
+                    <input type="number" className="token-input" />
+                </div>
+
+                <span><input type="checkbox" name="Private" />Private</span>
+
+                <button>Swap</button>
+
+                <p>Slippage: {}</p>
             </div>
-
-            <span><input type="checkbox" name="Private" />Private</span>
-
-            <button>Swap</button>
-
-            <p>Slippage: {}</p>
-		</div>
+            <img src={image} style={{width:"24%"}} />
+        </main>
 	);
 };
 export default Swap;
