a07646b4f5ff63c46a4278fcc746203ff1ec1e53
Daniel <danielmaestreyepes@gmail.com>
2022-02-22 20:58:38 +0100

    coinflip image added, still have to fix the spacing between the 2 elements and the img size

--- src/components/Coinflip/Coinflip.js
+++ src/components/Coinflip/Coinflip.js
@@ -1,29 +1,42 @@
 import React from "react";
 import "./Coinflip.css";
+import image from "../../assets/Elements/coinflip-pyramid.png";
 
 const Coinflip = () => {
-	return (
-		<div className="coinflip-div">
-			<h1><span style={{color:"#d70a84"}}>Head </span><span>/ Tail</span></h1>
+  return (
+    <main className="coinflip-container-main">
+      <div className="coinflip-div">
+        <h1>
+          <span style={{ color: "#d70a84" }}>Head </span>
+          <span>/ Tail</span>
+        </h1>
 
-            <div className="below-head">
-                <p>selected <span>{"Head"}</span></p>
-                <p>reward <span>{"20,000"}</span></p>
-            </div>
+        <div className="below-head">
+          <p>
+            selected <span>{"Head"}</span>
+          </p>
+          <p>
+            reward <span>{"20,000"}</span>
+          </p>
+        </div>
 
-            <p>Please set a bet amount</p>
+        <p>Please set a bet amount</p>
 
-            <div className="coinflip-calc">
-                <h1>−</h1>
-                <input type="text" pattern="[^a-zA-Z]+" />
-                <span>OWL</span>
-                <h1>+</h1>
-            </div>
+        <div className="coinflip-calc">
+          <h1>−</h1>
+          <input type="text" pattern="[^a-zA-Z]+" />
+          <span>OWL</span>
+          <h1>+</h1>
+        </div>
 
-            <button>Place bet</button>
+        <button>Place bet</button>
 
-            <p>Transaction ID: {}</p>
-		</div>
-	);
+        <p>Transaction ID: {}</p>
+      </div>
+      <div className="coinflip-image-container">
+        <img src={image} style={{width:"30%"}} />
+      </div>
+    </main>
+  );
 };
 export default Coinflip;
