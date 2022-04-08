59b698eb7915e630f79a3ee3680e0950e220102a
PC & Electronic Basics <admin@pe-basics.com>
2022-04-08 02:35:59 +0200

    Tweaked up coming soon page

--- src/components/ComingSoon/ComingSoon.js
+++ src/components/ComingSoon/ComingSoon.js
@@ -26,13 +26,16 @@ function ComingSoon() {
 					</div>
 					<div id="icons-wrapper">
 						<a href="https://twitter.com/NightOwlCasino" target="_blank" className="icon-wrapper">
-							<div id="twitter-icon" className="icon"></div>
+							<div id="twitter-icon-blue" className="icon-color"></div>
+							<div id="twitter-icon-white" className="icon-white"></div>
 						</a>
 						<a href="https://discord.com/invite/W69GTHe3pJ" target="_blank" className="icon-wrapper">
-							<div id="discord-icon" className="icon"></div>
+							<div id="discord-icon-blue" className="icon-color"></div>
+							<div id="discord-icon-white" className="icon-white"></div>
 						</a>
 						<a href="https://linktr.ee/NightOwlCasino" target="_blank" className="icon-wrapper">
-							<div id="linktree-icon" className="icon"></div>
+							<div id="linktree-icon-green" className="icon-color"></div>
+							<div id="linktree-icon-white" className="icon-white"></div>
 						</a>
 					</div>
 				</div>
