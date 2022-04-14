e0c498055c01f606a4d5e9677f09adfa1c16b050
PC & Electronic Basics <admin@pe-basics.com>
2022-04-14 11:44:46 +0200

    Added ui for coinflip

--- src/components/Games/Coinflip/Coinflip.js
+++ src/components/Games/Coinflip/Coinflip.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Coinflip.css";
-import image from "../../../assets/Elements/coinflip-pyramid.png";
-import { HiPlus, HiMinus } from "react-icons/hi";
-import { ImHammer2 } from "react-icons/im";
 
 const Coinflip = () => {
+
+	const [selection_state] = useState(false);
+
 	return (
-		<div id="coinflip-wrapper">
-			<div id="coinflip-coin">
-				
+		<div id="coinflip-content-wrapper">
+			<div id="coinflip-content">
+				<div id="coinflip-header-wrapper">
+					<div id="coinflip-header">
+						<label onClick={() => selection_state(true)} className="side-selector" htmlFor="head-side-selector">
+							<input name="coin-side-selector" id="head-side-selector" type="radio" value="head" checked={selection_state}  />
+							<h1 className="side-selector-name">Head</h1>
+						</label>
+						<h1 id="spacer">/</h1>
+						<label onClick={() => selection_state(false)} className="side-selector" htmlFor="tail-side-selector">
+							<input name="coin-side-selector" id="tail-side-selector" type="radio" value="tail" checked={selection_state} />
+							<h1 className="side-selector-name">Tail</h1>
+						</label>
+					</div>
+				</div>
+				<div id="currently-selected-wrapper">
+					<div id="currently-selected">
+						<span className="color-pink">Selected</span>
+						<span> </span>
+						<span id="currently-selected-placeholder">
+							<span className={selection_state ? "" : "hiden"} id="placeholder-head">Head</span>
+							<span className={selection_state ? "hiden" : ""} id="placeholder-tail">Tail</span>
+						</span>
+					</div>
+					<div id="reward">
+						<span>
+							<span  className="color-pink">Reward </span><span id="reward-amount">20000</span>
+						</span>
+					</div>
+				</div>
+				<div className="wrapper">
+					<span>Please select a bet amount</span>
+				</div>
+				<div id="input-wrapper">
+					<div id="input">
+						<div className="bet-button-change" id="decrease-bet">-</div>
+						<input />
+						<span id="input-unit" className="color-pink">OWL</span>
+						<div className="bet-button-change" id="increase-bet">+</div>
+					</div>
+				</div>
+				<div id="bet-button-wrapper">
+					<button>Place bet</button>
+				</div>
+				<div id="transaction-id-wrapper">
+					<span>Transaction ID:</span>
+					<span id="last-transaction-id">...</span>
+				</div>
 			</div>
 		</div>
 	);
 };
+
 export default Coinflip;
