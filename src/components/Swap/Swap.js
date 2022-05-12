1729acd39c2fa1ff7b7640bc12ae1fe69ae30f3a
Derrick DaCosta <dacosta89111@gmail.com>
2022-05-12 11:32:29 -0400

    Add utxo checks for the minimum erg and update proxy to point to localhost

--- src/components/Swap/Swap.js
+++ src/components/Swap/Swap.js
@@ -7,6 +7,8 @@ import StateContext from "../Context";
 const TOKENID_NO_TEST = "afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae933032";
 const TOKENID_FAKE_SIGUSD = "96c402c0e658909aa03f534006124f0e43725c467dbc8dea39680d0861892de5";
 const TOKENID_ERG  = "0000000000000000000000000000000000000000000000000000000000000000";
+const FEE_VALUE = 1100000;
+const MIN_BOX_VALUE = 1000000;
 
 function Swap({setIsLoading,setSwapTransaction}) {
     const [swap1, setSwap1] = useState(false);
@@ -18,80 +20,114 @@ function Swap({setIsLoading,setSwapTransaction}) {
     const backend = process.env.BACKEND_FQDN || "localhost";
 
     const swapTokens = (e) => {
-        e.preventDefault();
-		console.log("swap button pressed");
+      e.preventDefault();
+      console.log("swap button pressed");
 
-		// get input boxes for each token ID
-		const sigUSDAmount = 10;
-		const owl = 10;
+      // get input boxes for each token ID
+      const sigUSDAmount = 10;
+      const owl = 10;
 
-		ergoWallet
-			.get_utxos(swap1, TOKENID_FAKE_SIGUSD)
-			.then((utxosResponse) => {
-		//ergoWallet.get_utxos(swap2, TOKENID_NO_TEST).then(utxosResponse => {
-				if (utxosResponse.length === 0) {
-					console.log("NO UTXOS");
-				} else {
-					// send token input boxes and token amounts in a POST message to the backend
-                    setIsLoading(true);
-					axios
-						.post(`http://${backend}:8088/api/v1/swap/sigusd`, {
-					//axios.post(`http://${backend}:8088/api/v1/swap/owl`, {
-							amnt: swap1,
-							senderAddr: localStorage.getItem('walletAddress'),
-							utxos: utxosResponse,
-						})
-						.then(async function (response) {
-							const signedTx = await signTx(response.data);
-							console.log("signedTx", signedTx);
-							const txId = await submitTx(signedTx);
-							if (!txId) {
-								console.log(`No submitted tx ID`);
-								return null;
-							}
-                            setIsLoading(false);
-                            setSwapTransaction(txId);
-							console.log(`Transaction submitted - ${txId}`);
-						})
-						.catch(function (error) {
-							console.log(error);
-						});
-				}
-			});
-	    };
+      let utxos = []
+      // the minimum ERG requires a swap box, fee box, and change box
+      const minERG = MIN_BOX_VALUE + MIN_BOX_VALUE + FEE_VALUE
+
+      // Get utxo for ERGs
+      ergoWallet.get_utxos(minERG, TOKENID_ERG).then((utxosResponse) => {
+          if (utxosResponse.length === 0) {
+            console.log("NO ERG UTXOS")
+            return
+          } else {
+            utxos = JSON.parse(JSON.stringify(utxosResponse));
+            // This is a hack for now, we need to remove the decimals for SigUSD
+            const sigAmnt = swap1 * 100
+            ergoWallet
+              .get_utxos(swap1, TOKENID_FAKE_SIGUSD)
+              .then((utxosResponse) => {
+              //ergoWallet.get_utxos(swap2, TOKENID_NO_TEST).then(utxosResponse => {
+            if (utxosResponse.length === 0) {
+              console.log("NO SigUSD UTXOS");
+              return
+            } else {
+              utxosResponse.forEach(sigBox => {
+                let found = false
+                utxos.forEach(box => {
+                  // Check if any matching boxIds
+                  // TODO: Add a break/continue
+                  if (sigBox.boxId == box.boxId) {
+                    found = true
+                  }
+                })
+                // Found none
+                if (!found) {
+                  utxos.push(sigBox)
+                }
+              })
+              console.log(utxos)
+              // send token input boxes and token amounts in a POST message to the backend
+              setIsLoading(true);
+              axios
+                .post(`/api/v1/swap/sigusd`, {
+                //axios.post(`/api/v1/swap/owl`, {
+                  amnt: sigAmnt,
+                  senderAddr: localStorage.getItem('walletAddress'),
+                  utxos: utxos,
+                })
+                .then(async function (response) {
+                  const signedTx = await signTx(response.data);
+                  console.log("signedTx", signedTx);
+                  const txId = await submitTx(signedTx);
+                  if (!txId) {
+                    console.log(`No submitted tx ID`);
+                    return null;
+                  }
+                  setIsLoading(false);
+                  setSwapTransaction(txId);
+                  console.log(`Transaction submitted - ${txId}`);
+                })
+                .catch(function (error) {
+                  setIsLoading(false);
+                  console.log(error);
+                });
+            }
+            });
+          }
+      })
+    };
 
     async function signTx(txToBeSigned) {
-		try {
-			return await ergoWallet.sign_tx(txToBeSigned);
-		} catch (err) {
-			const msg = `[signTx] Error: ${JSON.stringify(err)}`;
-			console.error(msg, err);
-			return null;
-		}
-	}
+    try {
+      return await ergoWallet.sign_tx(txToBeSigned);
+    } catch (err) {
+      setIsLoading(false);
+      const msg = `[signTx] Error: ${JSON.stringify(err)}`;
+      console.error(msg, err);
+      return null;
+    }
+  }
 
     async function submitTx(txToBeSubmitted) {
-		try {
-			return await ergoWallet.submit_tx(txToBeSubmitted);
-		} catch (err) {
-			const msg = `[submitTx] Error: ${JSON.stringify(err)}`;
-			console.error(msg, err);
-			return null;
-		}
-	}
+    try {
+      return await ergoWallet.submit_tx(txToBeSubmitted);
+    } catch (err) {
+      setIsLoading(false);
+      const msg = `[submitTx] Error: ${JSON.stringify(err)}`;
+      console.error(msg, err);
+      return null;
+    }
+  }
 
     const handleSwapSelect = (option) => {
-        setSwapSelect1(option);
+      setSwapSelect1(option);
     }
     
     const handleSwap1 = (value) => {
-        setSwap1(value);
-        setSwap2(value*100);
+      setSwap1(value);
+      setSwap2(value*100);
     }
     
     const handleSwap2 = (value) => {
-        setSwap2(value);
-        setSwap1(value/100)
+      setSwap2(value);
+      setSwap1(value/100)
     }
 
     const convertOwl = () => {
