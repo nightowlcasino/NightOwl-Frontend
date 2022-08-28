b0629846689d1a34ae921af68af85fb2bbf97063
supremedacos <nightowlcasino@protonmail.com>
2022-08-27 20:21:31 -0400

    set port 9222 for nats endpoint

--- src/config/env.js
+++ src/config/env.js
@@ -14,15 +14,15 @@ const environments = {
     rngEndpoint: process.env.REACT_APP_RNG_ENDPOINT || "http://127.0.0.1:8089/api/v1/test/random-number",
   },
   development: {
-    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io",
+    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io:9222",
     rngEndpoint: process.env.REACT_APP_RNG_ENDPOINT || "https://dev.nightowlcasino.io/api/v1/random-number",
   },
   staging: {
-    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io",
+    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io:9222",
     rngEndpoint: process.env.REACT_APP_RNG_ENDPOINT || "https://staging.nightowlcasino.io/api/v1/random-number",
   },
   default: {
-    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io",
+    natsEndpoint: process.env.REACT_APP_NATS_ENDPOINT || "wss://nats.nightowlcasino.io:9222",
     rngEndpoint: process.env.REACT_APP_RNG_ENDPOINT || "https://dev.nightowlcasino.io/api/v1/random-number",
   }
 }
